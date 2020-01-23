751c9f97f59e1cebbf85fd463161eda6b19e50d3
ankit <ankitkejriwaal@gmail.com>
2020-01-23 02:42:51 -0500

    Added command line code that will execute racetool

--- src/microservices/Flask_Archer.py
+++ src/microservices/Flask_Archer.py
@@ -9,13 +9,16 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        arr = task_content.split()
-        result = run(arr, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-        if(result.returncode == 1):
-            str = result.stderr
-        else:
-            str = result.stdout
-        print(str)
+        # cmd_list = ["clang-archer DRB104-nowait-barrier-orig-no.c -o myApp -larcher","./myApp "]
+        cmd_list = ["pwd", "ls -l"]
+        for cmd in cmd_list:
+            arr = cmd.split()
+            result=run(arr, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+            if(result.returncode == 1):
+                str = result.stderr
+            else:
+                str = result.stdout
+            print(str)
         return render_template('index.html', val=str.split('\n'))
     else:
         return render_template('index.html', val="")
