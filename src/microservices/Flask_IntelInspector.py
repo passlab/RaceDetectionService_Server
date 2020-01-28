eb0be155f559a7257a46ed97a1ffb1bace768c93
ankit <ankitkejriwaal@gmail.com>
2020-01-27 19:36:59 -0500

    added the workflow to exec calls from 1 api call

--- src/microservices/Flask_IntelInspector.py
+++ src/microservices/Flask_IntelInspector.py
@@ -1,33 +1,15 @@
-
-from flask import Flask, request, render_template, redirect
 from subprocess import PIPE, run
-from werkzeug import secure_filename
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        arr = task_content.split()
+# list1 = sys.argv[1]
+# print(list1)
+# list2 = sys.argv[2]
+# print(list2)
+def intellinspector(file_name):
+    cmd_list = ["pwd", "ls -l " + file_name]
+    for cmd in cmd_list:
+        arr = cmd.split()
         result = run(arr, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         if(result.returncode == 1):
             str = result.stderr
         else:
             str = result.stdout
-        print(str)
-        return render_template('index.html', val=str.split('\n'))
-    else:
-        return render_template('index.html', val="")
-
-
-@app.route("/uploader", methods=['GET', 'POST'])
-def uploader():
-    if request.method == "POST":
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return redirect('/')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return str
--- src/microservices/Flask_TSan.py
+++ src/microservices/Flask_TSan.py
@@ -1,36 +1,15 @@
-
-from flask import Flask, request, render_template, redirect
 from subprocess import PIPE, run
-from werkzeug import secure_filename
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        # cmd_list = ["clang DRB001-antidep1-orig-yes.c -fopenmp -fsanitize=thread -fPIE -pie -g -o myApp", "./myApp "]
-        cmd_list = ["pwd", "ls -l"]
-        for cmd in cmd_list:
-            arr = cmd.split()
-            result = run(arr, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-            if(result.returncode == 1):
-                str = result.stderr
-            else:
-                str = result.stdout
-            print(str)
-        return render_template('index.html', val=str.split('\n'))
-    else:
-        return render_template('index.html', val="")
-
-
-@app.route("/uploader", methods=['GET', 'POST'])
-def uploader():
-    if request.method == "POST":
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return redirect('/')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# list1 = sys.argv[1]
+# print(list1)
+# list2 = sys.argv[2]
+# print(list2)
+def tsan(file_name):
+    cmd_list = ["pwd", "ls -l " + file_name]
+    for cmd in cmd_list:
+        arr = cmd.split()
+        result = run(arr, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        if(result.returncode == 1):
+            str = result.stderr
+        else:
+            str = result.stdout
+    return str
