ba293b0f01bd74a51ffb36cf763aad6d4c171859
Yaying Shi <yshi10@passlab-yaying.dyn.uncc.edu>
2020-01-29 18:56:09 -0500

    fix output parser

--- tools_output/outputParser.py
+++ tools_output/outputParser.py
@@ -43,6 +43,42 @@ for i in range(0,len(content)):
 						item["read file name"] = location[0]
 						item["read line #"] = location[1]
 						item["read symbol position"] = location[2]		
-
+	x = re.search("^Read",content[i])
+	if (x):
+		y = content[i].split()
+		index = y.index('at')
+		js = {}
+		js["Memory Address"] = y[index+1]
+		index = y.index('by')
+		Thread = re.split(":",y[index+2])
+		js["Write_thread"] = y[index+1] + ' ' + Thread[0]
+		y = content[i+1].split()
+		for item in y:
+			if(re.search("^/",item)):
+				file = item.rsplit("/",1)
+				js["file loaction"] = file[0]
+				location = file[1].split(":")
+				js["Read file name"] = location[0]
+				js["Read line #"] = location[1]
+				js["Read symbol position"] = location[2]
+		jsAry.append(js)
+	x = re.search("^Previous write",content[i])
+	if (x):
+		y = content[i].split()
+		index = y.index('at')
+		for item in jsAry:
+			if (item["Memory Address"] == y[index+1]):
+				index = y.index('by')
+				Thread = re.split(":",y[index+2])
+				item["Write_thread"] = y[index+1] + ' ' + Thread[0]
+				y = content[i+1].split()
+				for item1 in y:
+					if(re.search("^/",item1)):
+						file = item1.rsplit("/",1)
+						location = file[1].split(":")
+						item["write file name"] = location[0]
+						item["write line #"] = location[1]
+						item["write symbol position"] = location[2]
 for item in jsAry:
-	print(item)
+	r = json.dumps(item)
+	print(r)
