6129e8396e7ffc8fb60aa1edd246e791827fb049
Yaing Shi <Shiyaying9@gmail.com>
2020-03-09 00:01:23 -0400

    add majority vote

--- tools_output/Metaservice.py
+++ tools_output/Metaservice.py
@@ -11,17 +11,49 @@ d = json.loads(data)
 with open("inspectorOutput.json") as test:
 	data = test.read();
 
-d2 = json.loads(data)
+d1 = json.loads(data)
 
 with open("TsanOutput.json") as test:
 	data = test.read();
 
-d3 = json.loads(data)
-print(d)
-print(d2)
-print(d3)
-print(d['1']["tool"])
-print(d2['1']["tool"])
-print(d3['1']["tool"])
+d2 = json.loads(data)
 
-#need a function to analysis the input
+with open("rompOutput.json") as test:
+    data = test.read();
+
+d3 = json.loads(data)
+
+Voteflag = 0
+
+for i in range(len(d)):
+    if i is None:
+        ArcherVote = 0
+    else:
+        ArcherVote = 1
+        Voteflag += 1
+        
+for i in range(len(d1)):
+    if i is None:
+        InsepctorVote = 0
+    else:
+        InsepctorVote = 1
+        Voteflag += 1
+
+for i in range(len(d2)):
+    if i is None:
+        TsanVote = 0
+    else:
+        TsanVote = 1
+        Voteflag += 1
+
+for i in range(len(d3)):
+    if i is None:
+        RompVote = 0
+    else:
+        RompVote = 1
+        Voteflag += 1
+
+if Voteflag >= 2:
+    print("RDS detected a data race!")
+
+###information need to dispaly
